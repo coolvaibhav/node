5b8362c87a0628d64e2a1205d2619733be1f97cd
xoyal <vaibhavk32@gmail.com>
2017-02-21 11:16:10 +0530

    first time edit

--- common.php
+++ common.php
@@ -1,1 +1,4 @@
-<?php echo "Addded from server We require this file branch B-Two";?>
+<?php echo "Addded from server We require this file branch B-Two";
+
+echo "after adding the pulling the data";
+?>
